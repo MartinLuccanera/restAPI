940e1b10066b3726149791a99b2a7a2cfcc0fd17
Martin Luccanera <martin.luccanera@jivesoftware.com>
2018-09-07 00:14:17 -0300

    Adding some knowledge and fixing tracked folders

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,67 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    //Esto mapea todos los requests (GET POST PUT) ver como poner method=GET
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
 
-    @RequestMapping("/date")
+    /*@RequestMapping(value="/method8/{id:[\\d]+}/{name}")
+    @ResponseBody
+    public String method8(@PathVariable("id") long id, @PathVariable("name") String name){
+	return "method8 with id= "+id+" and name="+name;
+	*/
+    @RequestMapping(value="/date",
+            method={RequestMethod.POST,
+                    RequestMethod.GET},
+            produces={"application/json","application/xml"},
+            consumes="text/html")
     public Greeting greeting() {
         long millis=System.currentTimeMillis();
         java.util.Date date=new java.util.Date(millis);
         return new Greeting(counter.incrementAndGet(), date.toString());
     }
+
+    /* Get a JSON payload from a POST request
+    String payloadRequest = getBody(request);
+
+    Using this method
+
+    public static String getBody(HttpServletRequest request) throws IOException {
+
+    String body = null;
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = null;
+
+    try {
+        InputStream inputStream = request.getInputStream();
+        if (inputStream != null) {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            char[] charBuffer = new char[128];
+            int bytesRead = -1;
+            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                stringBuilder.append(charBuffer, 0, bytesRead);
+            }
+        } else {
+            stringBuilder.append("");
+        }
+    } catch (IOException ex) {
+        throw ex;
+    } finally {
+        if (bufferedReader != null) {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                throw ex;
+            }
+        }
+    }
+
+    body = stringBuilder.toString();
+    return body;
+}
+
+
+     */
 }
